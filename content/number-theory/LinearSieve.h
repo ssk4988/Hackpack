c2f54dab6061e3168c955a9b0ef3022c21be5099
Sachin S <ssk4988@gmail.com>
2023-10-17 18:58:38 -0400

    fix linear sieve

--- content/number-theory/LinearSieve.h
+++ content/number-theory/LinearSieve.h
@@ -10,8 +10,8 @@
 const int LIM = 1000000;
 vi lp(LIM+1), primes;
 
-rep(i, 2, LIM) {
+rep(i, 2, LIM + 1) {
 	if (lp[i] == 0) primes.push_back(lp[i] = i);
-	for (int j = 0; i * primes[j] <= LIM && primes[j] <= lp[i]; ++j) 
+	for (int j = 0; j < sz(primes) && i * primes[j] <= LIM && primes[j] <= lp[i]; ++j) 
 		lp[i * primes[j]] = primes[j];
 }
