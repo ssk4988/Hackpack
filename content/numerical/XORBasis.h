e1ea30c6df3d2f526753a2873ced8a146e6f5a8b
Sachin S <ssk4988@gmail.com>
2023-10-27 12:52:48 -0400

    xor basis update with bitsets

--- content/numerical/XORBasis.h
+++ content/numerical/XORBasis.h
@@ -1,32 +1,28 @@
 /**
  * Author: Sachin Sivakumar
  * Date: 2023-09-05
- * Description: maintains a basis in $\mathbb{Z}_2^d$, use longs for $B>32$
- * Time: $O(B)$
+ * Description: maintains a basis in $\mathbb{Z}_2^d$
+ * Time: $O(B^2/32)$
  * Status: works but not stress tested
  */
 
 #pragma once
 
-template<int B = 32>
+template<int B>
 struct XORBasis {
-	int basis[B] = {};
-	int nbasic = 0, nfree = 0;
-	bool indep(int v) {
-		for(int i = B - 1; i >= 0; i--)
-			if (v & (1 << i)) v ^= basis[i];
-		return v;
+	bitset<B> basis[B];
+	int npivot = 0, nfree = 0;
+	bool check(bitset<B> &v) {
+		rep(i, 0, B)
+			if (v.test(i)) v ^= basis[i];
+		return v.none();
 	}
-	void add(int v) {
-		for(int i = B - 1; i >= 0; i--)
-			if (v & (1 << i)) {
-				if (basis[i]) v ^= basis[i];
-				else {
-					basis[i] = v;
-					nbasic++;
-					return;
-				}
+	bool add(bitset<B> &v) {
+		rep(i, 0, B)
+			if (v.test(i)) {
+				if (basis[i].none()) return basis[i] = v, ++npivot;
+				v ^= basis[i];
 			}
-		nfree++;
+        return !++nfree;
 	}
 };
